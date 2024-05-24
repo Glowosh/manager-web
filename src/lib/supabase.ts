b0b48a36cc9e7ce18ba484641fd69c9d5bc2e8fc
Yago valuche <valuchedevs@gmail.com>
2024-05-24 09:59:28 -0300

    fix: Error when calling local environment variables

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,8 +1,10 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.VITE_REACT_APP_SUPABASE_URL as string;
-const supabaseAnonKey = import.meta.env
+const supabaseUrl = (import.meta as any).env
+  .VITE_REACT_APP_SUPABASE_URL as string;
+const supabaseAnonKey = (import.meta as any).env
   .VITE_REACT_APP_SUPABASE_ANON_KEY as string;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
